35bc84f07ec359081061c34e65d5cd3021644c2d
SteBaum <stephan.baum@edu.dsti.insitute>
2024-10-29 15:54:12 +0100

    feat: delete all resources network and storage pool

--- destroy.sh
+++ destroy.sh
@@ -7,5 +7,10 @@ source .env
 
 # Delete the VMs
 for f in edge-01 master-01 master-02 master-03 worker-01 worker-02 worker-03; do
+	echo "Deleting VM $f"
 	incus delete $f --force
 done
+
+# Delete storage pool and network 
+incus storage delete $storagepool
+incus network delete $network
